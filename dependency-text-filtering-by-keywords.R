b7c08bf8c0d0a11fabe2c80f3c88d0e9825752b5
Aashish Pandey <aashishkpandey@gmail.com>
2017-09-04 12:06:55 +0530

    correction

--- dependency-text-filtering-by-keywords.R
+++ dependency-text-filtering-by-keywords.R
@@ -1,8 +1,8 @@
-require(NLP) || install.packages("NLP")
-require(shiny) || install.packages("shiny")
-require(magrittr) || install.packages("magrittr")
-require(tidytext) || install.packages("tidytext")
-require(dplyr) || install.packages("dplyr")
+try(require(NLP) || install.packages("NLP"))
+try(require(shiny) || install.packages("shiny"))
+try(require(magrittr) || install.packages("magrittr"))
+try(require(tidytext) || install.packages("tidytext"))
+try(require(dplyr) || install.packages("dplyr"))
 
 library(NLP)
 library(shiny)
